02075ea80b5ba328ccf16e035077ec4dd962ddc0
Rajavel Maniguru Arunachalakani <35165507+rmavkani@users.noreply.github.com>
2021-07-13 16:17:43 +0530

    EnableGlobalMethodSecurity(prePostEnabled

--- src/main/java/com/seyon/security/ApplicationSecurityConfig.java
+++ src/main/java/com/seyon/security/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import static com.seyon.security.ApplicationUserRole.*;
 
 @Confguration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 
 private final PasswordEncoder passwordEncoder;  
